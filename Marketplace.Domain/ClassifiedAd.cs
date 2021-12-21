23c05a8520be2835b142aa264f21c71f494d5a3e
drsmile1001 <drsmile.1001@gmail.com>
2021-12-21 14:09:59 +0800

    ch5 分類廣告實例

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,17 @@
+﻿namespace Marketplace.Domain;
+public class ClassifiedAd
+{
+    public Guid Id { get; }
+
+    public ClassifiedAd(Guid id)
+    {
+        if (id == default)
+            throw new ArgumentException("id must be set!", nameof(id));
+        Id = id;
+    }
+
+    private Guid _ownerId;
+    private string _title;
+    private string _text;
+    private decimal _price;
+}
