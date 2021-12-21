07c43238f30f6c1ab493f995f67d17dbd7fe8114
drsmile1001 <drsmile.1001@gmail.com>
2021-12-21 16:42:19 +0800

    ch5 工廠

---
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -0,0 +1,28 @@
+using System.Text.RegularExpressions;
+
+namespace Marketplace.Domain;
+
+public record ClassifiedAdTitle
+{
+    public static ClassifiedAdTitle FromString(string title)
+        => new(title);
+
+    public static ClassifiedAdTitle FromHtml(string htmlTitle)
+    {
+        var supportedTagsReplaced = htmlTitle
+            .Replace("<i>", "*")
+            .Replace("</i>", "*")
+            .Replace("<b>", "**")
+            .Replace("</b>", "**");
+        return new ClassifiedAdTitle(Regex.Replace(supportedTagsReplaced, "<.*?>", string.Empty));
+
+    }
+    public string Value { get; }
+
+    private ClassifiedAdTitle(string value)
+    {
+        if (value.Length > 100)
+            throw new ArgumentOutOfRangeException(nameof(value), "Title cannot be longer than 100 characters");
+        Value = value;
+    }
+}
--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -1,10 +1,49 @@
+using System.Runtime.Serialization;
+
 namespace Marketplace.Domain;
 
-public record Money(decimal Amount)
+public record Money
 {
-    public Money Add(Money summand) => new(Amount + summand.Amount);
-    public Money Subtract(Money subtrahend) => new(Amount - subtrahend.Amount);
+    private const string DefaultCurrency = "EUR";
+    public static Money FromDecimal(decimal amount, string currency = DefaultCurrency)
+        => new(amount, currency);
+
+    public static Money FromString(string amount, string currency = DefaultCurrency)
+        => new(decimal.Parse(amount), currency);
+
+    protected Money(decimal amount, string currencyCode = DefaultCurrency)
+    {
+        if (decimal.Round(amount, 2) != amount)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount cannot have more than two decimals");
+
+        Amount = amount;
+        CurrencyCode = currencyCode;
+    }
+
+    public decimal Amount { get; }
+    public string CurrencyCode { get; }
+
+    public Money Add(Money summand)
+    {
+        if (CurrencyCode != summand.CurrencyCode)
+            throw new CurrencyMismatchException("Cannot sum amounts with different currencies");
+        return new(Amount + summand.Amount, CurrencyCode);
+    }
+    public Money Subtract(Money subtrahend)
+    {
+        if (CurrencyCode != subtrahend.CurrencyCode)
+            throw new CurrencyMismatchException("Cannot subtract amounts with different currencies");
+        return new(Amount - subtrahend.Amount, CurrencyCode);
+    }
 
     public static Money operator +(Money a, Money b) => a.Add(b);
     public static Money operator -(Money a, Money b) => a.Subtract(b);
+}
+
+[Serializable]
+internal class CurrencyMismatchException : Exception
+{
+    public CurrencyMismatchException(string? message) : base(message)
+    {
+    }
 }
