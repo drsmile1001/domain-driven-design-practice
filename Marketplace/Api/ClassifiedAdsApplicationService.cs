9a48e1b8848f4b0b4b6c0908f0d4810dfef153a2
drsmile1001 <drsmile.1001@gmail.com>
2021-12-22 16:47:10 +0800

    ch6 應用程式層

---
+++ Marketplace/Api/ClassifiedAdsApplicationService.cs
@@ -0,0 +1,8 @@
+namespace Marketplace.Api;
+public class ClassifiedAdsApplicationService
+{
+    public void Handle(Contracts.ClassifiedAds.V1.Create command)
+    {
+
+    }
+}
--- Marketplace/Api/ClassifiedAdsCommandsApi.cs
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -6,9 +6,16 @@ namespace Marketplace.Api;
 [Route("/ad")]
 public class ClassifiedAdsCommandsApi : ControllerBase
 {
+    private readonly ClassifiedAdsApplicationService _applicationService;
+    public ClassifiedAdsCommandsApi(ClassifiedAdsApplicationService applicationService)
+    {
+        _applicationService = applicationService;
+    }
+
     [HttpPost]
     public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
     {
+        _applicationService.Handle(request);
         return Ok();
     }
 }
