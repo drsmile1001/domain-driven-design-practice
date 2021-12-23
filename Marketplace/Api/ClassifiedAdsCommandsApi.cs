7578aa0bdd1c3ce960e5d36335380fc5acac8655
drsmile1001 <drsmile.1001@gmail.com>
2021-12-23 13:30:08 +0800

    ch6 應用程式服務 新增合約與api端口

--- Marketplace/Api/ClassifiedAdsCommandsApi.cs
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using static Marketplace.Contracts.ClassifiedAds;
 
 namespace Marketplace.Api;
 
@@ -13,9 +14,41 @@ public class ClassifiedAdsCommandsApi : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
+    public async Task<IActionResult> Post(V1.Create request)
     {
-        _applicationService.Handle(request);
+        await _applicationService.Handle(request);
+        return Ok();
+    }
+
+    [Route("name")]
+    [HttpPut]
+    public async Task<IActionResult> Put(V1.SetTitle request)
+    {
+        await _applicationService.Handle(request);
+        return Ok();
+    }
+
+    [Route("text")]
+    [HttpPut]
+    public async Task<IActionResult> Put(V1.UpdateText request)
+    {
+        await _applicationService.Handle(request);
+        return Ok();
+    }
+
+    [Route("price")]
+    [HttpPut]
+    public async Task<IActionResult> Put(V1.UpdatePrice request)
+    {
+        await _applicationService.Handle(request);
+        return Ok();
+    }
+
+    [Route("publish")]
+    [HttpPut]
+    public async Task<IActionResult> Put(V1.RequestToPublish request)
+    {
+        await _applicationService.Handle(request);
         return Ok();
     }
 }
--- Marketplace/Contracts/ClassifiedAds.cs
+++ Marketplace/Contracts/ClassifiedAds.cs
@@ -9,5 +9,30 @@ public static class ClassifiedAds
             public Guid Id { get; set; }
             public Guid OwnerId { get; set; }
         }
+
+        public class SetTitle
+        {
+            public Guid Id { get; set; }
+
+            public string Title { get; set; } = null!;
+        }
+
+        public class UpdateText
+        {
+            public Guid Id { get; set; }
+            public string Text { get; set; } = null!;
+        }
+
+        public class UpdatePrice
+        {
+            public Guid Id { get; set; }
+            public decimal Price { get; set; }
+            public string Currency { get; set; } = null!;
+        }
+
+        public class RequestToPublish
+        {
+            public Guid Id { get; set; }
+        }
     }
 }
