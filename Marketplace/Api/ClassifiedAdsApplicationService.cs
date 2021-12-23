58112afc61a9ad37966e684db3e29b98e4300796
drsmile1001 <drsmile.1001@gmail.com>
2021-12-23 13:14:08 +0800

    ch6 應用程式服務 將應用程式服務改成handle object

--- Marketplace/Api/ClassifiedAdsApplicationService.cs
+++ Marketplace/Api/ClassifiedAdsApplicationService.cs
@@ -1,8 +1,17 @@
-namespace Marketplace.Api;
-public class ClassifiedAdsApplicationService
-{
-    public void Handle(Contracts.ClassifiedAds.V1.Create command)
-    {
+using Marketplace.Framework;
+using static Marketplace.Contracts.ClassifiedAds;
 
+namespace Marketplace.Api;
+public class ClassifiedAdsApplicationService : IApplicationService
+{
+    public async Task Handle(object command)
+    {
+        switch (command)
+        {
+            case V1.Create cmd:
+                break;
+            default:
+                throw new InvalidOperationException($"Command type {command.GetType().FullName} is unknown");
+        }
     }
 }
